1757f17b7a316a5cf4a45708f375bb00a4934da1
sanaboina prasad <sssrkvp@gmail.com>
2018-11-13 07:44:51 +0530

    Initial Commit

---
+++ tableViewPratice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewPratice
+//
+//  Created by Sriram Prasad on 13/11/18.
+//  Copyright © 2018 FullStackNet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
